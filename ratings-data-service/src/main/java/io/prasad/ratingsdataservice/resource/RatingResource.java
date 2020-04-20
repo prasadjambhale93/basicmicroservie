cb916d23190a8385011ce782004141e39670e9d5
unknown <prasadjambhale93@gmail.com>
2020-04-20 22:15:29 +0100

    Added Ratings resources in rating project

---
+++ ratings-data-service/src/main/java/io/prasad/ratingsdataservice/resource/RatingResource.java
@@ -0,0 +1,16 @@
+package io.prasad.ratingsdataservice.resource;
+
+import io.prasad.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, 4);
+    }
+}
